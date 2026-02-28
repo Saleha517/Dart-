5b8b0ee2dc80ade65b9629a06df12dd1a9527dd3
Saleha517 <salehausman517@gmail.com>
2026-02-28 04:16:57 -0800

    Loops , for , while , do while

---
+++ Class03/index.dart
@@ -0,0 +1,41 @@
+void main(){
+
+  //loops
+
+  for(int i= 0; i < 5; i++){
+     print(i);
+  }
+
+  //nested loop
+
+  for(int j= 0; j < 5; j++){
+   for(int k=1; k<5; k++){
+      print('j: $j k: $k');
+   }
+  }
+  
+  // int o = 0;
+
+  // while (o <=o) {
+  //   print(o);
+
+  //   o++;
+  // } //infinite loop
+
+  int h = 0;
+
+  while (h <10) {
+    print(h);
+    h++;
+  }
+
+  int g = 1;
+
+  do {
+    print(g);
+  } while (g >=20);
+
+}
+
+
+ 
